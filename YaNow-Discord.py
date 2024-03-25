fb9af6e5d4703882264deb3f64f4e1a477c5adfd
Egor <141219716+chebupelka10@users.noreply.github.com>
2024-03-25 20:56:59 +0300

    Update YaNow-Discord.py

--- YaNow-Discord.py
+++ YaNow-Discord.py
@@ -2,13 +2,38 @@ from winsdk.windows.media.control import \
     GlobalSystemMediaTransportControlsSessionManager as MediaManager
 from pypresence import Presence
 import time, asyncio
+from pypresence.exceptions import PipeClosed
 import os
 
 THUMBNAIL_BUFFER_SIZE = 5 * 1024 * 1024
 CID = '1221371159510253598'
 
 rpc = Presence(CID)
-rpc.connect()
+
+def run():
+    while True:
+        try:
+            rpc.connect()
+            os.system("cls")
+            print('[\033[01;38;05;46m•\033[m] Connected to Discord')
+            time.sleep(2)
+            print("The script was launched successfully (by y9chepux)")
+            time.sleep(2)
+            os.system('cls')
+            print(" ")
+            print("███ █  █ █▀▀ █▀▀█ █  █ ▀▄ ▄▀")
+            print("█   █▀▀█ █▀▀ █  █ █  █   █  ")
+            print("███ █  █ █▄▄ █▀▀▀ ▀▄▄▀ ▄▀ ▀▄")
+            print(" ")
+            print(" ")
+            print("The code started without errors!")
+            print("Support: https://t.me/y9chepux/")
+            break
+        except:
+            os.system("cls")
+            print('[\033[38;05;226m•\033[m] Waiting discord running to connect...')
+            time.sleep(10)
+            continue
 
 async def get_media_info():
     sessions = await MediaManager.request_async()
@@ -46,30 +71,25 @@ buttons = [
 
 media = ''
 title = ''
-print("Connecting to discord app")
-time.sleep(1)
-print("Connected")
-time.sleep(2)
-print("The script was launched successfully (by y9chepux)")
-time.sleep(1)
-os.system('cls')
-print(" ")
-print("███ █  █ █▀▀ █▀▀█ █  █ ▀▄ ▄▀")
-print("█   █▀▀█ █▀▀ █  █ █  █   █  ")
-print("███ █  █ █▄▄ █▀▀▀ ▀▄▄▀ ▄▀ ▀▄")
-print(" ")
-print(" ")
-print("The code started without errors!")
-print("Support: https://t.me/y9chepux/")
+
+
+run()
+
 while True:
     try:
-        if title != asyncio.run(get_media_info())["title"]:
-            title = asyncio.run(get_media_info())["title"]
-            media = asyncio.run(get_media_info())
-            f = str(asyncio.run(get_player_info())).replace(".", " ").title()
-            rpc.update(details=f'Слушает Яндекс Музыку', state=f'{media["artist"]} - {media["title"]}', large_image='yamusic-icon', large_text='UwU', buttons=buttons, start=time.time())
-        else:
+        try:
+            if title != asyncio.run(get_media_info())["title"]:
+                title = asyncio.run(get_media_info())["title"]
+                media = asyncio.run(get_media_info())
+                f = str(asyncio.run(get_player_info())).replace(".", " ").title()
+                rpc.update(details=f'Слушает Яндекс Музыку', state=f'{media["artist"]} - {media["title"]}', large_image='yamusic-icon', large_text='UwU', buttons=buttons, start=time.time())
+            else:
+                time.sleep(1)
+        except TypeError:
+            rpc.clear()
             time.sleep(1)
-    except:
-        rpc.clear()
-        time.sleep(1)
+    except PipeClosed:
+        os.system("cls")
+        print('[\033[38;05;196m•\033[m] Connection to Discord lost, restartning...')
+        time.sleep(3)
+        run()
